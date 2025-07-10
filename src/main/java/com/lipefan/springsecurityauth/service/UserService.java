7d0b501095ae1d0af70010cd65984d2ec28a3038
Felipe Oliveira <lipefan0@gmail.com>
2025-07-10 12:08:29 -0300

    config: Adiciona criptografia na senha ao salvar usuario

--- src/main/java/com/lipefan/springsecurityauth/service/UserService.java
+++ src/main/java/com/lipefan/springsecurityauth/service/UserService.java
@@ -3,6 +3,7 @@ package com.lipefan.springsecurityauth.service;
 import com.lipefan.springsecurityauth.entity.User;
 import com.lipefan.springsecurityauth.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,8 +11,11 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public User save(User user) {
+        String password = user.getPassword();
+        user.setPassword(passwordEncoder.encode(password));
        return userRepository.save(user);
     }
 
