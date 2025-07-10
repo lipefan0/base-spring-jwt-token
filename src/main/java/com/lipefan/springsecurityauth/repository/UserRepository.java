ca900982f7519ff02ba9803ceb7a1156087a5459
Felipe Oliveira <lipefan0@gmail.com>
2025-07-10 14:25:13 -0300

    feat: Adiciona servico que localiza o usuario com base no token para autorizar nas rotas bloqueadas

--- src/main/java/com/lipefan/springsecurityauth/repository/UserRepository.java
+++ src/main/java/com/lipefan/springsecurityauth/repository/UserRepository.java
@@ -2,8 +2,12 @@ package com.lipefan.springsecurityauth.repository;
 
 import com.lipefan.springsecurityauth.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<UserDetails> findUserByEmail(String email);
 }
---
+++ src/main/java/com/lipefan/springsecurityauth/service/AuthService.java
@@ -0,0 +1,23 @@
+package com.lipefan.springsecurityauth.service;
+
+import com.lipefan.springsecurityauth.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findUserByEmail(username).orElseThrow(
+                () -> new UsernameNotFoundException("Usuário não encontrado: " + username)
+        );
+    }
+
+}
