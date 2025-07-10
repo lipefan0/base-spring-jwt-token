8fac93e39c5c106046801a8fd28e98f02e250fad
Felipe Oliveira <lipefan0@gmail.com>
2025-07-10 11:22:38 -0300

    initial commit

---
+++ src/test/java/com/lipefan/springsecurityauth/SpringSecurityAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lipefan.springsecurityauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
